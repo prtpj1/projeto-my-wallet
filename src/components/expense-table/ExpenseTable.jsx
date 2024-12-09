7a47ba386c4eeac30b19f825c0b1911b3a2c3120
prtpj1 <prtpj1@gmail.com>
2024-12-09 19:21:07 -0300

    :wrench: fix:(ExpenseTable.jsx) Warning: validateDOMNesting :arrow_right: move the row buttons to inside a <td> to fix this warning msg: "Warning: validateDOMNesting(...): <div> cannot appear as a child of <tr>"

--- src/components/expense-table/ExpenseTable.jsx
+++ src/components/expense-table/ExpenseTable.jsx
@@ -21,23 +21,25 @@ const ExpenseTable = ({ expenses, handleClickDelete, handleClickEdit }) => (
                 * (+exp.exchangeRates[exp.currency].ask)).toFixed(2)}
             </td>
             <td>Real</td>
-            <div className="table__btns">
-              <button
-                className="table__btn btn__editar"
-                onClick={ () => handleClickEdit(exp) }
-                type="button"
+            <td>
+              <div className="table__btns">
+                <button
+                  className="table__btn btn__editar"
+                  onClick={ () => handleClickEdit(exp) }
+                  type="button"
 
-              >
-                <FaRegEdit size={ 20 } />
-              </button>
-              <button
-                className="table__btn btn__excluir"
-                type="button"
-                onClick={ () => handleClickDelete(exp.id) }
-              >
-                <FaTrashAlt size={ 18 } />
-              </button>
-            </div>
+                >
+                  <FaRegEdit size={ 20 } />
+                </button>
+                <button
+                  className="table__btn btn__excluir"
+                  type="button"
+                  onClick={ () => handleClickDelete(exp.id) }
+                >
+                  <FaTrashAlt size={ 18 } />
+                </button>
+              </div>
+            </td>
           </tr>
         ))}
       </tbody>
